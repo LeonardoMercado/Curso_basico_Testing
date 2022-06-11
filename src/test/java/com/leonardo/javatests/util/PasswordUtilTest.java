8f29820011bae2b523ede3c4d3b1d2fb1c724ac3
Leonardo Fabio Mercado Benítez <lfmercadobe@unal.edu.co>
2022-06-11 18:46:53 -0500

    Se agrega junit a los test de StringUtilTest

--- src/test/java/com/leonardo/javatests/util/PasswordUtilTest.java
+++ src/test/java/com/leonardo/javatests/util/PasswordUtilTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PasswordUtilTest {
-    private String password = "leonard";
 
     @Test
     public void wake_when_has_less_than_8_letters() {
@@ -16,12 +15,10 @@ public class PasswordUtilTest {
     public void wake_when_has_only_letters() {
         assertEquals(SecurityLevel.WEAK, PasswordUtil.assessPassword("leonardoFabioMercadoBenitez"));
     }
-
     @Test
     public void medium_when_has_more_than_8_letters_and_less_than_15() {
         assertEquals(SecurityLevel.MEDIUM, PasswordUtil.assessPassword("leonard1235"));
     }
-
     @Test
     public void strong_when_has_more_than_15_letters() {
         assertEquals(SecurityLevel.STRONG, PasswordUtil.assessPassword("leo%&name#$\""));
--- src/test/java/com/leonardo/javatests/util/StringUtilTest.java
+++ src/test/java/com/leonardo/javatests/util/StringUtilTest.java
@@ -1,18 +1,20 @@
 package com.leonardo.javatests.util;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class StringUtilTest {
-    public static void main(String[] args) {
-        String consumo = StringUtil.repeat("leonardo",3);
-        if(consumo.equals("leonardo|leonardo|leonardo|")){
-            System.out.println("Test1: PASS");
-        }else{
-            System.out.println("Test2: FAIL");
-        }
-        String consumo2 = StringUtil.repeat("leonardo",5);
-        if(consumo2.equals("leonardo|leonardo|leonardo|leonardo|leonardo|")){
-            System.out.println("Test2: PASS");
-        }else{
-            System.out.println("Test2: FAIL");
-        }
+
+    @Test
+    public void contiene_6_repeticiones(){
+        String esperado = "leonardo|leonardo|leonardo|leonardo|leonardo|leonardo|";
+        assertEquals(esperado,StringUtil.repeat("leonardo",6));
     }
+    @Test
+    public void contiene_3_repeticiones(){
+        String esperado = "leonardo|leonardo|leonardo|";
+        assertEquals(esperado,StringUtil.repeat("leonardo",4));
+    }
+
 }
