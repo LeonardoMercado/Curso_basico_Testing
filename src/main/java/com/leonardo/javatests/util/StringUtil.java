6688dd8636f9b501bd59894b239d9d4b457f1494
Leonardo Fabio Mercado Benítez <lfmercadobe@unal.edu.co>
2022-06-11 19:22:47 -0500

    Se agrega la función de prueba para cuando tenemos párametro times negativos

--- src/main/java/com/leonardo/javatests/util/StringUtil.java
+++ src/main/java/com/leonardo/javatests/util/StringUtil.java
@@ -2,6 +2,9 @@ package com.leonardo.javatests.util;
 
 public class StringUtil {
     public static String repeat(String str,int times){
+        if(times<0){
+            throw new IllegalArgumentException("No negative times pls");
+        }
         String resultado = "";
         for (int i = 0; i < times; i++) {
             resultado += str + "|";
--- src/test/java/com/leonardo/javatests/util/StringUtilTest.java
+++ src/test/java/com/leonardo/javatests/util/StringUtilTest.java
@@ -14,7 +14,12 @@ public class StringUtilTest {
     @Test
     public void contiene_3_repeticiones(){
         String esperado = "leonardo|leonardo|leonardo|";
-        assertEquals(esperado,StringUtil.repeat("leonardo",4));
+        assertEquals(esperado,StringUtil.repeat("leonardo",3));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void argumento_negativo(){
+        assertEquals("leonardo",StringUtil.repeat("leonardo",-1));
     }
 
 }
