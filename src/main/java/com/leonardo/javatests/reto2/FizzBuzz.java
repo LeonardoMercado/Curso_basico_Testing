7819ca078f2a86cee23756af4f407cbf3eccd5c1
Leonardo Fabio Mercado Benítez <lfmercadobe@unal.edu.co>
2022-06-17 00:28:40 -0500

    Se hace refactoring de la solución planteada para el reto No 2

--- src/main/java/com/leonardo/javatests/reto2/FizzBuzz.java
+++ src/main/java/com/leonardo/javatests/reto2/FizzBuzz.java
@@ -3,14 +3,16 @@ package com.leonardo.javatests.reto2;
 public class FizzBuzz {
 
     public static String fizzBuzzCalculator(int number){
-        if(number%3==0 && number%5==0){
+        if (number % 3 != 0 || number % 5 != 0) {
+            if (number%3==0) {
+                return "Fizz";
+            } else if (number%5==0) {
+                return "Buzz";
+            }else{
+                return String.valueOf(number);
+            }
+        } else {
             return "FizzBuzz";
-        } else if (number%3==0) {
-            return "Fizz";
-        } else if (number%5==0) {
-            return "Buzz";
-        }else{
-            return String.valueOf(number);
         }
     }
 }
