42d656e9400e355b55a0c3043323ff5ce8c12aa3
Leonardo Fabio Mercado Benítez <lfmercadobe@unal.edu.co>
2022-06-16 23:44:03 -0500

    Se desarrolla el método para comprobar que un año es bisiesto utilizando TDD, paso test rojo y test verde

---
+++ src/main/java/com/leonardo/javatests/util/DateUtil.java
@@ -0,0 +1,18 @@
+package com.leonardo.javatests.util;
+
+public class DateUtil {
+
+    public static boolean isLeapYear(int year){
+        if(year%4==0 && year%100!=0){
+            return true;
+        }
+        if(year%4!=0){
+            return false;
+        }
+        if(year%400==0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
