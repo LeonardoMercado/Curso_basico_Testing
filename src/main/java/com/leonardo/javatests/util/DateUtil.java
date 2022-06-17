1bd056821be800a1522f1c61691d04e64a8f5a16
Leonardo Fabio Mercado Benítez <lfmercadobe@unal.edu.co>
2022-06-16 23:47:59 -0500

    Se simplifica aún más el método que calcula si un año es bisiesto o no

--- src/main/java/com/leonardo/javatests/util/DateUtil.java
+++ src/main/java/com/leonardo/javatests/util/DateUtil.java
@@ -3,10 +3,6 @@ package com.leonardo.javatests.util;
 public class DateUtil {
 
     public static boolean isLeapYear(int year){
-        if((year%4==0 && year%100!=0) || year%400==0){
-            return true;
-        }else{
-            return false;
-        }
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 }
