ae307279035363f984034004b213c100fb75caab
Leonardo Fabio Mercado Benítez <lfmercadobe@unal.edu.co>
2022-06-21 08:52:19 -0500

    Se agrega el código necesario para el testeo de movies

---
+++ src/main/java/com/leonardo/javatests/movies/data/MovieRepository.java
@@ -0,0 +1,12 @@
+package com.leonardo.javatests.movies.data;
+
+import com.leonardo.javatests.movies.model.Movie;
+
+import java.util.Collection;
+
+public interface MovieRepository {
+
+    Movie findById(long id);
+    Collection<Movie> findAll();
+    void saveOrUpdate(Movie movie);
+}
---
+++ src/main/java/com/leonardo/javatests/movies/model/Genre.java
@@ -0,0 +1,6 @@
+package com.leonardo.javatests.movies.model;
+
+public enum Genre {
+
+    ACTION, COMEDY, DRAMA, HORROR, THRILLER
+}
